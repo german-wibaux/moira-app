44c6f7dabb214d7874d9e62bf40af1521429365f
Yerman <germanwibaux@gmail.com>
2019-08-04 16:27:31 -0300

    Admin completo y ultimo tramo de front

--- src/app/components/new-article/new-article.component.ts
+++ src/app/components/new-article/new-article.component.ts
@@ -52,11 +52,49 @@ export class NewArticleComponent implements OnInit {
   }
 
 
-  /*Edit a property*/
+  /*Edit a article*/
   // editProperty(event, property: PropertyInterface) {
   //   this.editState = true;
   //   this.propertyToEdit = property;
   // }
+
+  uploadFile(event) {
+    // console.log(event);
+
+    const file = event.target.files[0];
+
+
+    if (file.type.split('/')[0] !== 'image') {
+      console.error('unsupported file type dalmiro-app')
+      return;
+    }
+    let loading = true;
+    const day = new Date();
+    const path = 'properties/' + day.getTime() + '/' + file.name;
+    const customMetadata = { app: 'chalita-app' };
+    const ref = this.storage.ref(path);
+    this.task = this.storage.upload(path, file, { customMetadata });
+    this.uploadPercent = this.task.percentageChanges();
+
+
+    return from(this.task).pipe(
+      switchMap(() => ref.getDownloadURL()),
+      tap(url => {
+        // use url here, e.g. assign it to a model  
+        this.article.images.splice(0,0,url);
+      }),
+      finalize(() => loading = false)
+    ).subscribe(() => {
+      // success
+      console.log(this.article.images);
+    }, error => {
+      // failure
+      console.log(error);
+    });    
+
+    //*********************Codigo pa la barra de progreso y algo mas si pinta************************
+    //     <div>{{ uploadPercent | async }}</div>
+  }
 
   onSaveArticle() {
     this.articlesService.addArticle(this.article).then( response => {
@@ -70,5 +108,169 @@ export class NewArticleComponent implements OnInit {
     
     //this.router.navigate(['/private']);
   }
+
+  uploadFile1(event) {
+    // console.log(event);
+    if (!this.article.images[0] || this.article.images[0] == 'borrado') {
+      alert("Cargar foto anterior");
+    } else { 
+
+      const file = event.target.files[0];
+
+      if (file.type.split('/')[0] !== 'image') {
+        console.error('unsupported file type dalmiro-app')
+        return;
+      }
+      let loading = true;
+      const day = new Date();
+      const path = 'properties/' + day.getTime() + '/' + file.name;
+      const customMetadata = { app: 'chalita-app' };
+      const ref = this.storage.ref(path);
+      this.task = this.storage.upload(path, file, { customMetadata });
+      this.uploadPercent1 = this.task.percentageChanges();
+
+
+      return from(this.task).pipe(
+        switchMap(() => ref.getDownloadURL()),
+        tap(url => {
+          // use url here, e.g. assign it to a model        
+            this.article.images.splice(1,0,url);                
+        }),
+        finalize(() => loading = false)
+      ).subscribe(() => {
+        // success
+        console.log(this.article.images);
+      }, error => {
+        // failure
+        console.log(error);
+      }); 
+    }
+    //*********************Codigo pa la barra de progreso y algo mas si pinta************************
+    //     <div>{{ uploadPercent | async }}</div>
+  }
+
+  uploadFile2(event) {
+    // console.log(event);
+
+    if (!this.article.images[1] || this.article.images[1] == 'borrado') {
+      alert("Cargar foto anterior");
+    } else { 
+
+    const file = event.target.files[0];
+
+
+    if (file.type.split('/')[0] !== 'image') {
+      console.error('unsupported file type dalmiro-app')
+      return;
+    }
+    let loading = true;
+    const day = new Date();
+    const path = 'properties/' + day.getTime() + '/' + file.name;
+    const customMetadata = { app: 'chalita-app' };
+    const ref = this.storage.ref(path);
+    this.task = this.storage.upload(path, file, { customMetadata });
+    this.uploadPercent2 = this.task.percentageChanges();
+
+
+    return from(this.task).pipe(
+      switchMap(() => ref.getDownloadURL()),
+      tap(url => {
+        // use url here, e.g. assign it to a model       
+        this.article.images.splice(2,0,url);        
+      }),
+      finalize(() => loading = false)
+    ).subscribe(() => {
+      // success
+    }, error => {
+      // failure
+      console.log(error);
+    });   
+  }
+    //*********************Codigo pa la barra de progreso y algo mas si pinta************************
+    //     <div>{{ uploadPercent | async }}</div>
+  }
+
+  uploadFile3(event) {
+    // console.log(event);
+
+    if (!this.article.images[2] || this.article.images[2] == 'borrado') {
+      alert("Cargar foto anterior");
+    } else { 
+
+    const file = event.target.files[0];
+
+
+    if (file.type.split('/')[0] !== 'image') {
+      console.error('unsupported file type dalmiro-app')
+      return;
+    }
+    let loading = true;
+    const day = new Date();
+    const path = 'properties/' + day.getTime() + '/' + file.name;
+    const customMetadata = { app: 'chalita-app' };
+    const ref = this.storage.ref(path);
+    this.task = this.storage.upload(path, file, { customMetadata });
+    this.uploadPercent3 = this.task.percentageChanges();
+
+
+    return from(this.task).pipe(
+      switchMap(() => ref.getDownloadURL()),
+      tap(url => {
+        // use url here, e.g. assign it to a model       
+        this.article.images.splice(3,0,url);        
+      }),
+      finalize(() => loading = false)
+    ).subscribe(() => {
+      // success
+    }, error => {
+      // failure
+      console.log(error);
+    });   
+  }
+    //*********************Codigo pa la barra de progreso y algo mas si pinta************************
+    //     <div>{{ uploadPercent | async }}</div>
+  }
+
+  uploadFile4(event) {
+    // console.log(event);
+
+    if (!this.article.images[3] || this.article.images[3] == 'borrado') {
+      alert("Cargar foto anterior");
+    } else { 
+
+    const file = event.target.files[0];
+
+
+    if (file.type.split('/')[0] !== 'image') {
+      console.error('unsupported file type dalmiro-app')
+      return;
+    }
+    let loading = true;
+    const day = new Date();
+    const path = 'properties/' + day.getTime() + '/' + file.name;
+    const customMetadata = { app: 'chalita-app' };
+    const ref = this.storage.ref(path);
+    this.task = this.storage.upload(path, file, { customMetadata });
+    this.uploadPercent4 = this.task.percentageChanges();
+
+
+    return from(this.task).pipe(
+      switchMap(() => ref.getDownloadURL()),
+      tap(url => {
+        // use url here, e.g. assign it to a model       
+        this.article.images.splice(4,0,url);        
+      }),
+      finalize(() => loading = false)
+    ).subscribe(() => {
+      // success
+    }, error => {
+      // failure
+      console.log(error);
+    });   
+  }
+    //*********************Codigo pa la barra de progreso y algo mas si pinta************************
+    //     <div>{{ uploadPercent | async }}</div>
+  }
+
 
 }
